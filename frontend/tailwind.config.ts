3c3b8d3e16f31ddd5076d1ba4db561d6e3ad8821
faiz140405 <cahmargablok51@gmail.com>
2025-12-04 02:24:01 +0700

    tambah fitur gruping dan filtering

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,35 @@
+// frontend/tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+      },
+      // --- TAMBAHKAN INI ---
+      keyframes: {
+        gradient: {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+      },
+      animation: {
+        gradient: 'gradient 6s ease infinite', // Durasi 6 detik, looping selamanya
+      },
+      // ---------------------
+    },
+  },
+  plugins: [],
+};
+export default config;
